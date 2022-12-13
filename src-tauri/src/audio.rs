94ccf661730b5d0761aeb05a709440357fa0aef9
minetake <minetake01.m@gmail.com>
2022-12-13 13:24:28 +0900

    [add] デコード結果を1チャンネルに変換する処理を追加 [add] デバッグ用にエンコーダーを追加

--- src-tauri/src/audio.rs
+++ src-tauri/src/audio.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 
 use hound::WavSpec;
+use itertools::Itertools;
 
 #[derive(Debug, Default)]
 pub struct AudioEditor {
@@ -9,19 +10,40 @@ pub struct AudioEditor {
 }
 impl AudioEditor {
     pub fn decode(&mut self, path: PathBuf) -> Result<(), String> {
-        let mut reader = match hound::WavReader::open(path) {
+        let reader = match hound::WavReader::open(path) {
             Ok(reader) => reader,
             Err(err) => {
                 return Err(format!("ファイルを読み込めませんでした。\n{}", err));
             },
         };
-        self.spec = Some(reader.spec());
-        self.samples = match reader.samples().collect::<Result<Vec<i32>, _>>() {
+
+        let mut spec = reader.spec();
+        let raw_channels = spec.channels;
+
+        //デコード
+        let samples = match reader.into_samples().collect::<Result<Vec<i32>, _>>() {
             Ok(samples) => samples,
             Err(err) => {
                 return Err(format!("デコードに失敗しました。\n{}", err));
             },
         };
+
+        //1チャンネルに変換(それぞれのチャンネルを比較して一番小さい値を採用)
+        self.samples = samples.chunks(raw_channels as usize)
+            .map(|chunk| chunk.iter().min().unwrap().to_owned())
+            .collect::<Vec<i32>>();
+        spec.channels = 1;
+        self.spec = Some(spec);
+
         Ok(())
+    }
+
+    //デバッグ用エンコーダー
+    pub fn encode (&self) {
+        let mut writer = hound::WavWriter::create("output.wav", self.spec.unwrap()).unwrap();
+        self.samples.iter().for_each(|sample| {
+            writer.write_sample(*sample).unwrap();
+        });
+        writer.finalize().unwrap();
     }
 }
