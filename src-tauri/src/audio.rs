700b45703b29db3cec307213e256496f230db6ea
minetake <minetake01.m@gmail.com>
2022-12-23 20:24:14 +0900

    [add] ファイル選択、デコード、分割のコマンドを追加

--- src-tauri/src/audio.rs
+++ src-tauri/src/audio.rs
@@ -1,20 +1,22 @@
-use std::path::PathBuf;
-
 use hound::WavSpec;
+use itertools::Itertools;
 
-#[derive(Debug, Default)]
+#[derive(Debug, Clone, Default)]
 pub struct AudioEditor {
     pub spec: Option<WavSpec>,
     pub samples: Vec<i32>,
 }
 impl AudioEditor {
-    pub fn decode(&mut self, path: PathBuf) -> Result<(), String> {
+    pub fn decode(&mut self, path: String) -> Result<(), String> {
         let reader = match hound::WavReader::open(path) {
             Ok(reader) => reader,
             Err(err) => {
                 return Err(format!("ファイルを読み込めませんでした。\n{}", err));
             },
         };
+
+        //クリア
+        self.samples = vec![];
 
         let spec = reader.spec();
         self.spec = Some(spec);
@@ -41,12 +43,44 @@ impl AudioEditor {
     }
 
     pub fn samples_extraction(&self, start: usize, end: usize, n: f32) -> Vec<i32> {
-        let chunk_size = (self.samples.len() as f32 / n).ceil() as i32;
+        let chunk_size = ((end - start) as f32 / n).ceil() as i32;
         let extracted: Vec<i32> = self.samples[start..end].chunks(chunk_size as usize).map(|chunk| {
             chunk.iter().map(|sample| sample.abs()).sum::<i32>() / chunk_size
         })
         .collect();
 
         extracted
+    }
+
+    pub fn split_range(&self, threshold: i32, talk_dur_sec: f32, mute_dur_sec: f32, extend_sec: f32) -> Vec<Vec<usize>> {
+        let sample_rate = self.spec.unwrap().sample_rate;
+        let talk_dur = (sample_rate as f32 * talk_dur_sec) as usize;
+        let mute_dur = (sample_rate as f32 * mute_dur_sec) as usize;
+        let extend = (sample_rate as f32 * extend_sec) as usize;
+
+        let mut ranges: Vec<Vec<usize>> = vec![];
+        let mut range: [usize; 2] = [0, 0];
+        let mut last_key = false;
+        for (key, mut group) in &self.samples.iter().enumerate().group_by(|(_, sample)| sample.abs() >= threshold) {
+            let index = group.next().unwrap().0;
+            let len = group.count() + 1;
+            if key && !last_key {
+                range[0] = index;
+                last_key = true;
+            } else if !key && len >= mute_dur && index != 0 {
+                range[1] = index - 1;
+                last_key = false;
+
+                if range[1] - range[0] >= talk_dur {
+                    ranges.push(vec![range[0].saturating_sub(extend), range[1].saturating_add(extend)]);
+                    range = [0, 0];
+                }
+            }
+        }
+        if range[1].saturating_sub(range[0]) >= talk_dur {
+            ranges.push(vec![range[0] - extend, range[1] + extend]);
+        }
+
+        ranges
     }
 }
