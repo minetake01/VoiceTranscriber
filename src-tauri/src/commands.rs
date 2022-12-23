7434cf22da164211c89c7b8ac19f535f9a00137c
minetake <minetake01.m@gmail.com>
2022-12-23 19:53:18 +0900

    [add] ファイル選択、デコード、分割のコマンドを追加 [update] 状態管理をonce_cellからTauriに変更

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,13 +1,34 @@
-use crate::AUDIO_EDITOR;
+use std::path::PathBuf;
+
+use tauri::api::dialog::blocking::FileDialogBuilder;
+
+use crate::EditorState;
 
 #[tauri::command]
-pub fn samples_extraction(start: usize, end: i32, n: f32) -> Vec<i32> {
-    let audio_editor = AUDIO_EDITOR.lock().unwrap();
+pub fn select_file() -> Option<PathBuf> {
+    FileDialogBuilder::new().add_filter("WAV Audio File (VLC)", &["wav"]).pick_file()
+}
+
+#[tauri::command]
+pub async fn decode(state: tauri::State<'_, EditorState>, path: String) -> Result<(), String> {
+    let mut audio_editor = state.0.lock().unwrap();
+    audio_editor.decode(path)
+}
+
+#[tauri::command]
+pub async fn samples_extraction(state: tauri::State<'_, EditorState>, start: usize, end: i32, n: f32) -> Result<Vec<i32>, ()> {
+    let audio_editor = state.0.lock().unwrap();
     let arg_end: usize;
     if end == -1 {
         arg_end = audio_editor.samples.len();
     } else {
         arg_end = end as usize;
     }
-    audio_editor.samples_extraction(start, arg_end, n)
+    Ok(audio_editor.samples_extraction(start, arg_end, n))
+}
+
+#[tauri::command]
+pub async fn split_range(state: tauri::State<'_, EditorState>, threshold: i32, talk_dur_sec: f32, mute_dur_sec: f32, extend_sec: f32) -> Result<Vec<Vec<usize>>, ()> {
+    let audio_editor = state.0.lock().unwrap();
+    Ok(audio_editor.split_range(threshold, talk_dur_sec, mute_dur_sec, extend_sec))
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,19 +9,16 @@ mod audio;
 use std::sync::Mutex;
 
 use audio::AudioEditor;
-use once_cell::sync::Lazy;
 use tauri::{
     Menu,
     CustomMenuItem,
     Submenu,
-    api::dialog::{
-        blocking::{FileDialogBuilder, MessageDialogBuilder}, MessageDialogKind,
-    },
+    Manager,
 };
 
-use crate::commands::samples_extraction;
+use crate::commands::*;
 
-static AUDIO_EDITOR: Lazy<Mutex<AudioEditor>> = Lazy::new(|| Mutex::new(AudioEditor::default()));
+pub struct EditorState(Mutex<AudioEditor>);
 
 fn main() {
     let open_file = CustomMenuItem::new("open_file".to_owned(), "Open File...");
@@ -36,24 +33,18 @@ fn main() {
         .add_submenu(file);
 
     tauri::Builder::default()
+        .manage(EditorState(Default::default()))
         .invoke_handler(tauri::generate_handler![
-            samples_extraction
+            select_file,
+            decode,
+            samples_extraction,
+            split_range,
         ])
         .menu(menu)
         .on_menu_event(|event| {
             match event.menu_item_id() {
                 "open_file" => {
-                    //ファイル選択ダイアログ表示
-                    let Some(path) = FileDialogBuilder::new().add_filter("WAV Audio File (VLC)", &["wav"]).pick_file() else { return; };
-
-                    let mut audio_editor = AUDIO_EDITOR.lock().unwrap();
-
-                    if let Err(err) = audio_editor.decode(path) {
-                        MessageDialogBuilder::new("デコーダーエラー", format!("{}", err)).kind(MessageDialogKind::Error).show();
-                        return;
-                    }
-                    
-                    event.window().emit("decoded", audio_editor.spec.unwrap().sample_rate).unwrap();
+                    event.window().emit("file_selected", select_file()).unwrap();
                 }
                 _ => {}
             }
