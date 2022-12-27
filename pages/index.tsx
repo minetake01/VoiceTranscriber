a0209252d2b2b09efdd4c6f170535ed1acbb833b
noflm <takaashi1024@gmail.com>
2022-12-27 18:52:40 +0900

    [update] meta description content

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     return <>
         <Head>
             <title>Train data creator</title>
-            <meta name="description" content="Elite35P ServerのEliteVoiceProjectで使う、学習用音声データを作るためのGUIアプリ。" />
+            <meta name="description" content="Elite35P-ServerのEliteVoiceProjectで使用する、学習用音声データセットを効率的に制作する為のGUIアプリ。" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/favicon.ico" />
         </Head>
