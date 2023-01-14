7072263c404da536cf7368829690beb0034ea693
minetake <minetake01.m@gmail.com>
2023-01-15 08:56:17 +0900

    [change] アプリ名を「VoiceTranscriber」に変更

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Train data creator
+# VoiceTranscriber
 [Elite35P-Server](https://huggingface.co/Elite35P-Server/)の[EliteVoiceProject](https://huggingface.co/datasets/Elite35P-Server/EliteVoiceProject)で使用する、学習用音声データセットの作成を効率的に行う為のGUIアプリ。
 
 ## Usage
 ```bash
-git clone https://github.com/minetake01/train-data-creator
-cd train-data-creator
+git clone https://github.com/minetake01/VoiceTranscriber
+cd VoiceTranscriber
 pnpm install
 pnpm tauri dev
 ```
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { RecoilRoot } from 'recoil';
 export default function App({ Component, pageProps }: AppProps) {
     return <>
         <Head>
-            <title>Train data creator</title>
+            <title>VoiceTranscriber</title>
             <meta name="description" content="Elite35P-ServerのEliteVoiceProjectで使用する、学習用音声データセットを効率的に制作する為のGUIアプリ。" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
