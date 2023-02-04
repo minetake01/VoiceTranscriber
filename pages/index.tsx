75b55f1c41bbaa3eea0e6e5282b479c26fa570fd
minetake <minetake01.m@gmail.com>
2023-02-04 22:20:27 +0900

    [fix] ファイル選択、デコード処理中にボタンを操作不可能にするように修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,33 @@
 import { Button, Grid, Stack } from "@mui/material";
-import useSelectFile from "hooks/useSelectFile";
+import { invoke } from "@tauri-apps/api/tauri";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function FileSelect() {
-    const selectFile = useSelectFile();
-    
+    const router = useRouter();
+    const [processing, setProcessing] = useState(false);
+
     return <>
         <Grid container justifyContent="center" alignItems="center" height="100%">
             <Stack spacing={1} direction="row">
-                <Button variant="contained" onClick={() => selectFile("open_file", "/audio-split")}>
+                <Button variant="contained" disabled={processing} onClick={async () => {
+                    setProcessing(true);
+                    invoke("open_file").then(() => {
+                        router.push("/audio-split");
+                    }).finally(() => {
+                        setProcessing(false);
+                    });
+                }}>
                     ファイルを開く
                 </Button>
-                <Button variant="outlined" onClick={() => selectFile("open_project", "/labeling")}>
+                <Button variant="outlined" disabled={processing} onClick={async () => {
+                    setProcessing(true);
+                    invoke("open_project").then(() => {
+                        router.push("/labeling");
+                    }).finally(() => {
+                        setProcessing(false);
+                    });
+                }}>
                     プロジェクトを開く
                 </Button>
             </Stack>
